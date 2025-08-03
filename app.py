1df0547b277e9118d155223c4222126fb9c93b58
Sham kumar.S <shyam0015kumar@gmail.com>
2025-08-03 19:36:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,9 @@ def api_user():
         "donations": dummy_user['donations']
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
